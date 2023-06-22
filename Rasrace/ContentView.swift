1da9dc264927ef24bbd9698258d734592e94c018
Muhammad Rezky <mrezkysulihin@gmail.com>
2023-06-22 14:02:28 +0700

    Initial Commit

---
+++ Rasrace/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rasrace
+//
+//  Created by Muhammad Rezky on 22/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rasrace/RasraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RasraceApp.swift
+//  Rasrace
+//
+//  Created by Muhammad Rezky on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RasraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
